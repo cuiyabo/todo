981a4b2ecceb6a5e6b0f5c19161ab27b3edbf88f
cuiyabo <532883794@qq.com>
2016-11-07 19:00:20 +0800

    todo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(function(){
+	var arr=[];
+	
 	var pos;
 	$('#todolist').on('touchstart','input',function(e){
 		pos = e.originalEvent.changedTouches[0].clientX; 
@@ -17,22 +19,37 @@ $(function(){
 			arr.splice($(this).index(),1);
 			localStorage.obj = JSON.stringify(arr);
 //			$(this).remove();
+//			console.log(arr)
 		}
 	})
 	
-	var arr=[];
+	
 	var todolist=$('#todolist');
 	var li=$('#first');
 	if(localStorage.obj){
 		arr =JSON.parse(localStorage.obj)
 			for(var i=0;i<arr.length;i++){
-			$('<input>').html(arr[i]).appendTo('#todolist')
+			$('<input value="">').html(arr[i]).appendTo('#todolist')
 		}	
 	}
 //	console.log($('.aniu'))
+// 换颜色
+	function getRandomColor(){ 
+		var c = '#'; 
+		var cArray = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']; 
+		for(var i = 0; i < 6;i++){ 
+			var cIndex = Math.round(Math.random()*15); 
+			c += cArray[cIndex]; 
+		} 
+		return c; 
+	}	
+		
+		
+		 
+		
 	$('.aniu').on('click',function(){
 		localStorage.obj = JSON.stringify(arr);
-		$('<input>').html('').appendTo('#todolist')
+		$('<input>').css("background-color",getRandomColor()).html('').appendTo('#todolist')
 	})
 })
 
